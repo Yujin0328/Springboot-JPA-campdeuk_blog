5ab67d503ccf0eb3cdb90b04edad87458349a82a
Yujin0328 <ahile@naver.com>
2022-02-23 00:38:02 +0900

    환경세팅 완료

---
+++ src/test/java/com/campdeuk/campdeuk_blog/CampdeukBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campdeuk.campdeuk_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampdeukBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
